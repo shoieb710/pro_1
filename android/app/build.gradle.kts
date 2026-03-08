cf5242e997bafe43b9b485b3ed706b43cca44a5f
shoieb710 <shoieb710@gmail.com>
2026-03-08 02:48:48 +0200

    1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.pro_1"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = "29.0.14206865"
 
     compileOptions {
@@ -25,7 +25,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
